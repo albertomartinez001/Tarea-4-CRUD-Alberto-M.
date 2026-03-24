7578391e16e3d3d349a1fcb361cd1713a1b9f372
Alberto Martinez <alberto1230@gmail.com>
2026-03-24 12:18:43 -0600

    Basecommand ejecutado con exito y correccion de errores

--- universidad/management/commands/poblar_alumnos.py
+++ universidad/management/commands/poblar_alumnos.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from universidad.Models.alumno import Alumno  # Importante: verifica las mayúsculas/minúsculas de tus carpetas
+from universidad.Models.Alumno.models import Alumno
 from datetime import date
 import random
 
@@ -13,22 +13,22 @@ class Command(BaseCommand):
         alumnos_lista = []
 
         for i in range(10000):
-            # Creamos el objeto en memoria
+            # Creamos el objeto en memoria basado en nuestro modelo creado desde la tarea anterior
             nuevo_alumno = Alumno(
                 first_name=f"Nombre_{i}",
                 last_name=f"Apellido_{i}",
                 email=f"alumno.test.{i}@universidad.edu",
-                phone=f"1234{i:04d}", # Genera un número de relleno
+                phone=f"1234{i:04d}", # Genera un número de relleno para mantener mi formato
                 gender=random.choice(generos),
-                birth_date=date(2000, 1, 1), # Fecha fija para simplificar
+                birth_date=date(2000, 1, 1), # Fecha fija para simplificar y evitar problemas si se quiere validar despues 
                 is_active=True
             )
             alumnos_lista.append(nuevo_alumno)
 
-        # Usamos bulk_create para insertar TODO en una sola ejecución SQL
+        # Usamos bulk_create para insertar todo en una sola ejecución SQL
         # Esto es 100 veces más rápido que hacer .save() en cada vuelta del loop
         try:
-            Alumno.objects.bulk_create(alumnos_lista)
+            Alumno.objects.bulk_create(alumnos_lista, batch_size=500)
             self.stdout.write(self.style.SUCCESS(f'¡Éxito! Se han insertado {len(alumnos_lista)} alumnos.'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Hubo un error al insertar: {e}'))
